287f4ffc77ab3e13d316b4eb295d5eef7adebdfc
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-03-24 17:42:30 +0900

    add: theme settings

---
+++ src/libs/theme.js
@@ -0,0 +1,27 @@
+import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode("#f0e7db", "#202023")(props),
+    },
+  }),
+};
+
+const fonts = {
+  heading: "'M PLUS Rounded 1c'",
+};
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+};
+
+const theme = extendTheme({
+  config,
+  fonts,
+  styles,
+});
+
+export default theme;
