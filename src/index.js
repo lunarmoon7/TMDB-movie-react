31317c72f803dbba9157f9496f1ef386e9a83ec9
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-03-24 17:41:58 +0900

    add: ColorModeScript

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ColorModeScript } from '@chakra-ui/react';
+import theme from './libs/theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </React.StrictMode>
 );
