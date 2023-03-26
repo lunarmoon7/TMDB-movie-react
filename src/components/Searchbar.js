9083c7957b2551801a78b27521e53f1ef9820856
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-03-26 19:52:21 +0900

    refactor: Integerate component

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,12 +1,40 @@
-import { InputGroup, Input, InputLeftElement } from "@chakra-ui/react";
+import { InputGroup, Input, InputLeftElement, Box } from "@chakra-ui/react";
 import { SearchIcon, Search2Icon } from "@chakra-ui/icons";
+import TMDB from "../api/apicall";
+import { useRef, useState } from "react";
 
 const Searchbar = (props) => {
+  const keywordRef = useRef();
+  const [enteredKeyword, setEnteredKeyword] = useState("");
+
+  const changeHandler = (e) => {
+    e.preventDefault();
+    setEnteredKeyword(e.target.value);
+  };
+
+  const enterHandler = (e) => {
+    if (e.keyCode === 13) {
+      props.searchHandler(enteredKeyword, e.keyCode);
+      setEnteredKeyword("");
+    }
+  };
   return (
-    <InputGroup size='lg'>
-      <InputLeftElement pointerEvents='none' children={<SearchIcon color='gray.300'/>}/>
-      <Input type='text' placeholder="Search here..." borderRadius={50}/>
-    </InputGroup>
+    <Box>
+      <InputGroup size="lg">
+        <InputLeftElement
+          pointerEvents="none"
+          children={<SearchIcon color="gray.300" />}
+        />
+        <Input
+          type="text"
+          placeholder="Search here..."
+          borderRadius={50}
+          onChange={changeHandler}
+          onKeyDown={enterHandler}
+          ref={keywordRef}
+        />
+      </InputGroup>
+    </Box>
   );
 };
 
