b7d4b7f4b8023c7bbfe623179e61005d033931ee
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-03-26 16:46:48 +0900

    style: change component tree

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import TMDB from "./api/apicall";
 import { MovieItem } from "./components/grid-item";
 import MovieList from "./components/MovieList";
 import Navbar from "./components/navbar";
+import Section from "./components/Section";
+import Searchbar from "./components/Searchbar";
 
 const API_KEY = "69bea4bb33d62c0bf6b991c1b63dbbae";
 const URL = "https://api.themoviedb.org/3/movie/";
@@ -35,23 +37,34 @@ const App = () => {
   return (
     <ChakraProvider theme={theme}>
       <Navbar />
-      <Container maxW="container.md" pt={14}>
-        <MovieList>
-          {movies.map((movie) => (
-            <MovieItem
-              key={movie.id}
-              adult={movie.adult}
-              id={movie.id}
-              title={movie.title}
-              overview={movie.overview}
-              release_date={movie.release_date}
-              vote_average={movie.vote_average}
-              vote_count={movie.vote_count}
-              popularity={movie.popularity}
-              thumbnail={movie.poster_path}
-            />
-          ))}
-        </MovieList>
+
+      <Container maxW='contianer.lg' mt={100}>
+        <Section styles={{ marginTop: "50px" }}>
+          <Container maxW="container.lg">
+            <Searchbar />
+          </Container>
+        </Section>
+
+        <Section>
+          <Container maxW="container.lg" pt={70}>
+            <MovieList>
+              {movies.map((movie) => (
+                <MovieItem
+                  key={movie.id}
+                  adult={movie.adult}
+                  id={movie.id}
+                  title={movie.title}
+                  overview={movie.overview}
+                  release_date={movie.release_date}
+                  vote_average={movie.vote_average}
+                  vote_count={movie.vote_count}
+                  popularity={movie.popularity}
+                  thumbnail={movie.poster_path}
+                />
+              ))}
+            </MovieList>
+          </Container>
+        </Section>
       </Container>
     </ChakraProvider>
   );
