58ed3b58445e9f01e8f04c21f1b828b955db6f99
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-03-26 19:50:19 +0900

    chore: Add TMDB_SEARCH_URL

--- src/api/apikey.js
+++ src/api/apikey.js
@@ -1,7 +1,8 @@
 const config = {
   TMDB_API_KEY: "69bea4bb33d62c0bf6b991c1b63dbbae",
   TMDB_BASE_URL: "https://api.themoviedb.org/3/movie",
-  TMDB_POSTER_URL: "https://image.tmdb.org/t/p/original"
+  TMDB_POSTER_URL: "https://image.tmdb.org/t/p/original",
+  TMDB_SEARCH_URL: "https://api.themoviedb.org/3/search",
 };
 
 export default config;
