5d013c69cd7ad8889584dc4a38ce2919811c1211
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-03-26 15:22:31 +0900

    fix: promise error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import { ChakraProvider, Box, Flex, Container } from "@chakra-ui/react";
+import {
+  Center,
+  Text,
+  ChakraProvider,
+  Box,
+  Flex,
+  Container,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import theme from "./libs/theme";
 import ThemeToggleButton from "./components/theme-toggle-button";
 import TMDB from "./api/apicall";
+import { MovieItem } from "./components/grid-item";
+import MovieList from "./components/MovieList";
+import Navbar from "./components/navbar";
 
 const API_KEY = "69bea4bb33d62c0bf6b991c1b63dbbae";
 const URL = "https://api.themoviedb.org/3/movie/";
@@ -11,38 +21,40 @@ const App = () => {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
 
-  const getData = (callType) => {
+  const promiseHandler = (callType, setStateType) => {
     callType.then((data) => {
-      setMovies(data);
-      console.log(data);
+      setStateType(data);
     });
   };
 
   useEffect(() => {
-    getData(TMDB.getPopularMovies());
+    promiseHandler(TMDB.getPopularMovies(), setMovies);
+    console.log(movies);
   }, []);
 
   return (
     <ChakraProvider theme={theme}>
-      <Container textAlign="center" fontSize={50}>
-        TMDB
+      <Navbar />
+      <Container maxW="container.md" pt={14}>
+        <Flex color="black" textAlign="center" overflowX="auto">
+          <MovieList>
+            {movies.map((movie) => (
+              <MovieItem
+                key={movie.id}
+                adult={movie.adult}
+                id={movie.id}
+                title={movie.title}
+                overview={movie.overview}
+                release_date={movie.release_date}
+                vote_average={movie.vote_average}
+                vote_count={movie.vote_count}
+                popularity={movie.popularity}
+                thumbnail={movie.poster_path}
+              />
+            ))}
+          </MovieList>
+        </Flex>
       </Container>
-      <ThemeToggleButton />
-      {/* <Flex
-        boxSize={40}
-        m={2}
-        bg="tomato"
-        borderColor="gray.200"
-        align="center"
-        justify="space-around"
-      >
-        <Box as="button" borderRadius="md" bg="teal.100" color="white" boxSize={10}>
-          A
-        </Box>
-        <Box as="button" borderRadius="md" bg="gray.500" color="white" boxSize={10}>
-          B
-        </Box>
-      </Flex> */}
     </ChakraProvider>
   );
 };
