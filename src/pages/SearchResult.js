22ab4e466ea8a8fc5deb3740f29bef1f435241c2
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-03-26 19:53:25 +0900

    feat: Add SearchResult component(route component)

---
+++ src/pages/SearchResult.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { Box, Text } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import TMDB from "../api/apicall";
+
+const SearchResult = (props) => {
+  const { keyword } = useParams();
+  const [ret, setRet] = useState();
+
+  const promiseHandler = (callType, setStateType) => {
+    callType.then((data) => {
+      setStateType((prev) => {
+        return [...data];
+      });
+    });
+  };
+
+  useEffect(() => {
+      promiseHandler(TMDB.getSearchMovies(keyword), setRet);
+    // return () => {
+    //   TMDB.getSearchWithKeywords(keyword).then((data) => {
+    //     console.log(data);
+    //     setRet((prev) => {
+    //       return [...data];
+    //     });
+    //   });
+    // };
+  }, []);
+
+//   데이터 확인용
+  useEffect(() => {
+    console.log(ret);
+  }, [ret]);
+
+  return (
+    <Box>
+      <Text></Text>
+    </Box>
+  );
+};
+
+export default SearchResult;
