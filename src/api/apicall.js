4814dc7c5702e6c9b4ca819a6b1c9074a6da9d9a
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-03-26 15:22:37 +0900

    init

---
+++ src/api/apicall.js
@@ -0,0 +1,234 @@
+import config from "./apikey";
+
+const TMDB_API_KEY = config.TMDB_API_KEY;
+const TMDB_BASE_URL = config.TMDB_BASE_URL;
+const TMDB_POSTER_URL = config.TMDB_POSTER_URL;
+
+const TMDB = {
+  getPopularMovies: async () => {
+    const resp = await fetch(
+      `${TMDB_BASE_URL}/popular?api_key=${TMDB_API_KEY}`
+    );
+    const popularMovies = await resp.json();
+    // popularMovies.results = popularMovies.results.map((data) => ({
+    //   ...data,
+    //   adult: data.adult,
+    //   backdrop_path: data.backdrop_path,
+    //   genre_ids: data.genre_ids,
+    //   id: data.id,
+    //   original_language: data.original_language,
+    //   original_title: data.original_title,
+    //   overview: data.overview,
+    //   popularity: data.popularity,
+    //   poster_path: data.poster_path,
+    //   release_date: data.release_date,
+    //   title: data.title,
+    //   video: data.video,
+    //   vote_average: data.vote_average,
+    //   vote_count: data.vote_count,
+    // }));
+
+    return popularMovies.results;
+  },
+
+  getDetails: async (movie_id) => {
+    const resp = await fetch(
+      `${TMDB_BASE_URL}/${movie_id}?api_key=${TMDB_API_KEY}`
+    );
+    const details = await resp.json();
+
+    return details.results;
+  },
+
+  getImages: async (movie_id) => {
+    const resp = await fetch(
+      `${TMDB_BASE_URL}/${movie_id}/images?api_key=${TMDB_API_KEY}`
+    );
+    const images = await resp.json();
+
+    return images.results;
+  },
+
+  getRecommendations: async (movie_id) => {
+    const resp = await fetch(
+      `${TMDB_BASE_URL}/${movie_id}/recommendations?api_key=${TMDB_API_KEY}`
+    );
+    const recommendations = resp.json();
+
+    return recommendations;
+  },
+
+  getSimilarMovies: async (movie_id) => {
+    const resp = await fetch(
+      `${TMDB_BASE_URL}/${movie_id}/similar?api_key=${TMDB_API_KEY}`
+    );
+    const similarMovies = resp.json();
+
+    return similarMovies;
+  },
+
+  getUpComing: async () => {
+    const resp = await fetch(
+      `${TMDB_BASE_URL}/upcoming?api_key=${TMDB_API_KEY}`
+    );
+    const upcoming = resp.json();
+
+    return upcoming;
+  },
+
+  getTopRated: async () => {
+    const resp = await fetch(
+      `${TMDB_BASE_URL}/top_rated?api_key=${TMDB_API_KEY}`
+    );
+    const topRated = resp.json();
+    return topRated;
+  },
+
+  getNowPlaying: async () => {
+    const resp = await fetch(
+      `${TMDB_BASE_URL}/now_playing?api_key=${TMDB_API_KEY}`
+    );
+    const nowPlaying = resp.json();
+
+    return nowPlaying;
+  },
+
+  getLatestMovies: async () => {
+    const resp = await fetch(`${TMDB_BASE_URL}/latest?api_key=${TMDB_API_KEY}`);
+    const latestMovies = resp.json();
+
+    return latestMovies;
+  },
+
+  getTranslations: async (movie_id) => {
+    const resp = await fetch(
+      `${TMDB_BASE_URL}/${movie_id}/translations?api_key=${TMDB_API_KEY}`
+    );
+    const translations = resp.json();
+
+    return translations;
+  },
+
+  getReviews: async (movie_id) => {
+    const resp = await fetch(
+      `${TMDB_BASE_URL}/${movie_id}/reviews?api_key=${TMDB_API_KEY}`
+    );
+    const reviews = resp.json();
+
+    return reviews;
+  },
+
+  getKeywords: async (movie_id) => {
+    const resp = await fetch(
+      `${TMDB_BASE_URL}/${movie_id}/keywords?api_key=${TMDB_API_KEY}`
+    );
+    const keywords = resp.json();
+
+    return keywords;
+  },
+
+  getCredits: async (movie_id) => {
+    const resp = await fetch(
+      `${TMDB_BASE_URL}/${movie_id}/credits?api_key=${TMDB_API_KEY}`
+    );
+    const credits = resp.json();
+
+    return credits;
+  },
+};
+
+export default TMDB;
+
+// const call = {
+//     getPopularMovies: async (TMDB_BASE_URL, TMDB_API_KEY) => {
+//       const popularMovies = await (
+//         await fetch(`${TMDB_BASE_URL}/popular?TMDB_API_KEY=${TMDB_API_KEY}`)
+//       ).json();
+
+//       return popularMovies;
+//     },
+
+//     getDetails: async (TMDB_BASE_URL, TMDB_API_KEY, movie_id) => {
+//       const details = await (await fetch(`${TMDB_BASE_URL}/${movie_id}`)).json();
+
+//       return details;
+//     },
+
+//     getImages: async (TMDB_BASE_URL, TMDB_API_KEY, movie_id) => {
+//       const images = await (await fetch(`${TMDB_BASE_URL}/${movie_id}/images`)).json();
+
+//       return images;
+//     },
+
+//     getRecommendations: async (TMDB_BASE_URL, TMDB_API_KEY, movie_id) => {
+//       const recommendations = await (
+//         await fetch(`${TMDB_BASE_URL}/${movie_id}/recommendations`)
+//       ).json();
+
+//       return recommendations;
+//     },
+
+//     getSimilarMovies: async (TMDB_BASE_URL, TMDB_API_KEY, movie_id) => {
+//       const similarMovies = await (
+//         await fetch(`${TMDB_BASE_URL}/${movie_id}/similar`)
+//       ).json();
+
+//       return similarMovies;
+//     },
+
+//     getUpComing: async (TMDB_BASE_URL, TMDB_API_KEY) => {
+//       const upcoming = await (await fetch(`${TMDB_BASE_URL}/upcoming`)).json();
+
+//       return upcoming;
+//     },
+
+//     getTopRated: async (TMDB_BASE_URL, TMDB_API_KEY) => {
+//       const topRated = await (await fetch(`${TMDB_BASE_URL}/top_rated`)).json();
+
+//       return topRated;
+//     },
+
+//     getNowPlaying: async (TMDB_BASE_URL, TMDB_API_KEY) => {
+//       const nowPlaying = await (await fetch(`${TMDB_BASE_URL}/now_playing`)).json();
+
+//       return nowPlaying;
+//     },
+
+//     getLatestMovies: async (TMDB_BASE_URL, TMDB_API_KEY) => {
+//       const latestMovies = await (await fetch(`${TMDB_BASE_URL}/latest`)).json();
+
+//       return latestMovies;
+//     },
+
+//     getTranslations: async (TMDB_BASE_URL, TMDB_API_KEY, movie_id) => {
+//       const translations = await (
+//         await fetch(`${TMDB_BASE_URL}/${movie_id}/translations`)
+//       ).json();
+
+//       return translations;
+//     },
+
+//     getReviews: async (TMDB_BASE_URL, TMDB_API_KEY, movie_id) => {
+//       const reviews = await (
+//         await fetch(`${TMDB_BASE_URL}/${movie_id}/reviews`)
+//       ).json();
+
+//       return reviews;
+//     },
+
+//     getKeywords: async (TMDB_BASE_URL, TMDB_API_KEY, movie_id) => {
+//       const keywords = await (
+//         await fetch(`${TMDB_BASE_URL}/${movie_id}/keywords`)
+//       ).json();
+
+//       return keywords;
+//     },
+
+//     getCredits: async (TMDB_BASE_URL, TMDB_API_KEY, movie_id) => {
+//       const credits = await (
+//         await fetch(`${TMDB_BASE_URL}/${movie_id}/credits`)
+//       ).json();
+
+//       return credits;
+//     },
+//   };
---
+++ src/api/apikey.js
@@ -0,0 +1,7 @@
+const config = {
+  TMDB_API_KEY: "69bea4bb33d62c0bf6b991c1b63dbbae",
+  TMDB_BASE_URL: "https://api.themoviedb.org/3/movie",
+  TMDB_POSTER_URL: "https://image.tmdb.org/t/p/original"
+};
+
+export default config;
