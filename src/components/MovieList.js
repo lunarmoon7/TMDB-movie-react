95db3b4b0e528bbe6b7297cfc9c84f08482d24bd
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-03-26 19:51:39 +0900

    refactor: Integerate component

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,20 +1,45 @@
 import React from "react";
-import { Container, Flex, Box, SimpleGrid } from "@chakra-ui/react";
+import {
+  Container,
+  Flex,
+  Box,
+  SimpleGrid,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { MovieItem } from "./Movieitem";
+import ApiError from "./Apierror";
 
 const MovieList = (props) => {
   return (
-    <Flex
-      color="black"
-      textAlign="center"
-      overflowX="auto"
-      sx={{
-        "::-webkit-scrollbar": {
-          display: "none",
-        },
-      }}
-    >
-      {props.children}
-    </Flex>
+    <Box>
+      {!props.movies && <ApiError />}
+      {props.movies && <Flex
+        color="black"
+        textAlign="center"
+        overflowX="auto"
+        sx={{
+          "::-webkit-scrollbar": {
+            display: "none",
+          },
+        }}
+      >
+        {props.movies.map((movie) => (
+          <MovieItem
+            key={movie.id}
+            adult={movie.adult}
+            id={movie.id}
+            title={movie.title}
+            overview={movie.overview}
+            release_date={movie.release_date}
+            vote_average={movie.vote_average}
+            vote_count={movie.vote_count}
+            popularity={movie.popularity}
+            thumbnail={movie.poster_path}
+          />
+        ))}
+      </Flex>
+}
+    </Box>
   );
 };
 
