1bff0c07fdef13f102aa80dee5f9ab78daba9139
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-03-27 16:38:46 +0900

    feat: make up components

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -1,11 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { Box, Text } from "@chakra-ui/react";
-import { useParams } from "react-router-dom";
+import { Box, Text, ChakraProvider, Container } from "@chakra-ui/react";
+import {
+  useParams,
+  useNavigate,
+  useLocation,
+  useSearchParams,
+  setSearchParams,
+} from "react-router-dom";
 import TMDB from "../api/apicall";
+import { SearchSection } from "../components/Section";
+import { SearchList } from "../components/item-list";
+import theme from "../libs/theme";
+import Navbar from "../components/navbar";
+import Searchbar from "../components/Searchbar";
 
 const SearchResult = (props) => {
+  const navigate = useNavigate();
   const { keyword } = useParams();
-  const [ret, setRet] = useState();
+  const [searchResult, setSearchResult] = useState([]);
+  const location = useLocation();
 
   const promiseHandler = (callType, setStateType) => {
     callType.then((data) => {
@@ -15,19 +28,39 @@ const SearchResult = (props) => {
     });
   };
 
+  const searchHandler = (enteredKeyword, enteredKeyCode) => {
+    if (enteredKeyword.trim().length !== 0 && enteredKeyCode === 13) {
+      routePageHandler(enteredKeyword);
+    }
+  };
+
+  const routePageHandler = (enteredKeyword) => {
+    navigate(`/search/${enteredKeyword}`);
+    navigate(0);
+  };
+
   useEffect(() => {
-      promiseHandler(TMDB.getSearchMovies(keyword), setRet);
+    promiseHandler(TMDB.getSearchMovies(keyword), setSearchResult);
   }, []);
 
-//   데이터 확인용
+  // 데이터 확인용
   useEffect(() => {
-    console.log(ret);
-  }, [ret]);
+    console.log(searchResult);
+    console.log(keyword);
+  }, [searchResult]);
 
   return (
-    <Box>
-      <Text></Text>
-    </Box>
+    <ChakraProvider theme={theme}>
+      <Navbar />
+      <Container maxW="contianer.lg" mt={100}>
+        <Container maxW="container.lg">
+          <Searchbar searchHandler={searchHandler} />
+        </Container>
+        <SearchSection heading={searchResult.length}>
+          <SearchList searchResult={searchResult} />
+        </SearchSection>
+      </Container>
+    </ChakraProvider>
   );
 };
 
