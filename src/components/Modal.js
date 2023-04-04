389862425eb5234ee04d729886f9cd2aca33dc4c
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-04-04 16:25:10 +0900

    feat: Structure component tree

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -9,9 +9,35 @@ import {
   Button,
 } from "@chakra-ui/react";
 
-import { ModalItem } from "./item";
+import { useState, useEffect } from "react";
+import { ModalCreditItem, ModalDetailItem } from "./item";
+import {
+  ModalCreditSection,
+  ModalDetailSection,
+  ModalSection,
+  ModalSimilarMovieSection,
+} from "./Section";
+import { CreditList } from "./item-list";
+
+import TMDB from "../api/apicall";
+import config from "../api/apikey";
+
+const TMDB_POSTER_URL = config.TMDB_POSTER_URL;
 
 export const MovieModal = (props) => {
+  const [credits, setCredits] = useState([]);
+
+  const promiseHandler = (callType, setStateType) => {
+    callType.then((data) => {
+      setStateType((prev) => {
+        return { ...data };
+      });
+    });
+  };
+
+  useEffect(() => {
+    promiseHandler(TMDB.getCredits(props.id), setCredits);
+  }, []);
   return (
     <Modal onClose={props.onClose} isOpen={props.isOpen} size="xl">
       <ModalOverlay
@@ -19,21 +45,29 @@ export const MovieModal = (props) => {
         backdropFilter="auto"
         backdropBlur="10px"
       />
-      <ModalContent bg='blackAlpha.700' minW='900px'>
+      <ModalContent bg="blackAlpha.700" maxW="1000px">
         <ModalHeader></ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <ModalItem
-            adult={props.adult}
-            id={props.id}
-            title={props.title}
-            overview={props.overview}
-            release_date={props.release_date}
-            vote_average={props.vote_average}
-            vote_count={props.vote_count}
-            popularity={props.popularity}
-            thumbnail={props.thumbnail}
-          />
+          <ModalSection>
+            <ModalDetailSection>
+              <ModalDetailItem
+                adult={props.adult}
+                id={props.id}
+                title={props.title}
+                overview={props.overview}
+                release_date={props.release_date}
+                vote_average={props.vote_average}
+                vote_count={props.vote_count}
+                popularity={props.popularity}
+                thumbnail={props.thumbnail}
+              />
+            </ModalDetailSection>
+            <ModalCreditSection>
+              <CreditList creditResult={credits['cast']}/>
+            </ModalCreditSection>
+            <ModalSimilarMovieSection />
+          </ModalSection>
         </ModalBody>
         <ModalFooter>
           {/* <Button onClick={props.onClose}>Close</Button> */}
