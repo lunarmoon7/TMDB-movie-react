6952cf5d188d8c8ecb2b10a39f2ee51a7b1ff64a
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-04-07 14:53:59 +0900

    styles: Add color scheme

--- src/components/Genre.js
+++ src/components/Genre.js
@@ -1,31 +1,36 @@
 import { Badge, Box } from "@chakra-ui/react";
 
 const genres = {
-    '28' : "Action",
-    '12' : "Adventure",
-    '16' : "Animation",
-    '35' : "Comedy",
-    '80' : "Crime",
-    '99' : "Documentary",
-    '18' : "Drama",
-    '10751' : "Family",
-    '14' : "Fantasy",
-    '36' : "History",
-    '27' : "Horror",
-    '10402' : "Music",
-    '9648' : "Mystery",
-    '10749' : "Romance",
-    '878' : "SF",
-    '10770' : "TV Movie",
-    '53' : "Thriller",
-    '10752' : "War",
-    '37' : "Western",
+  28: { type: "Action", color: "teal" },
+  12: { type: "Adventure", color: "blue" },
+  16: { type: "Animation", color: "yellow" },
+  35: { type: "Comedy", color: "purple" },
+  80: { type: "Crime", color: "red" },
+  99: { type: "Documentary", color: "gray" },
+  18: { type: "Drama", color: "yellow" },
+  10751: { type: "Family", color: "pink" },
+  14: { type: "Fantasy", color: "purple" },
+  36: { type: "History", color: "yellow" },
+  27: { type: "Horror", color: "gray" },
+  10402: { type: "Music", color: "yellow" },
+  9648: { type: "Mystery", color: "purple" },
+  10749: { type: "Romance", color: "pink" },
+  878: { type: "SF", color: "cyan" },
+  10770: { type: "TV Movie", color: "whiteAlpha" },
+  53: { type: "Thriller", color: "red" },
+  10752: { type: "War", color: "yellow" },
+  37: { type: "Western", color: "yellow" },
 };
-  
+
 export const Genre = (props) => (
   <Box>
-    <Badge borderRadius="full" px="3" variant='outline' colorScheme="purple">
-      {genres[props.id]}
+    <Badge
+      borderRadius="full"
+      px="3"
+      variant="outline"
+      colorScheme={genres[props.id]["color"]}
+    >
+      {genres[props.id]["type"]}
     </Badge>
   </Box>
 );
