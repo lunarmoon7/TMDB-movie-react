ac3eec54df213b6b4b53567280c74d6e4ffbb7ec
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-04-07 15:21:21 +0900

    styles: change style

--- src/components/upcomingDate.js
+++ src/components/upcomingDate.js
@@ -1,7 +1,7 @@
 import { Badge, Box } from "@chakra-ui/react";
 
-export const upcomingDate = (props) => (
-  <Box position="absolute" zIndex="1" top="2%" right="5%">
+export const UpcomingDate = (props) => (
+  <Box position="absolute" zIndex="1" top="2%" left="5%">
     <Badge borderRadius="full" px="2" bg="blackAlpha.700" color="white">
         {props.minimum} ~ {props.maximum}
     </Badge>
